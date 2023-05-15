db479e2cd2b4a4ad9c448d506738ddc2ce6b5590
mmi2004 <iqbalmujtaba10@gmail.com>
2023-05-14 23:52:55 -0400

    edit to summary

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -163,8 +163,8 @@ const Row3 = () => {
           <Typography variant="h6" margin="0 1rem">
           According to the financial statements of XYZ Corporation, the company 
           generated a total revenue of $283,000 and a total profit of $212,000 
-          for the fiscal year. Overall, the financial data suggests that XYZ Corporation had a successful year, 
-           with strong revenue growth and a high profit margin. 
+          for the fiscal year. Our data suggests that XYZ had a successful year with strong revenue growth
+          and a high profit margin. 
           </Typography>
         </DashboardBox>
     </>
